2234a7f639713ae0d3d8144fed3954abf67e72e8
Guilherme Abtibol <Guilhermeabtibol@users.noreply.github.com>
2025-11-08 01:57:57 -0400

    chore: inicializ projeto Spring Boot

---
+++ financecontrol/src/test/java/com/abtiboltech/financecontrol/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abtiboltech.financecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
