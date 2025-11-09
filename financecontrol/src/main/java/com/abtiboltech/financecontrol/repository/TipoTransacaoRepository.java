6293b09dc95550bae2cfc795363406552fc86ae8
Guilherme Abtibol <Guilhermeabtibol@users.noreply.github.com>
2025-11-09 00:28:18 -0400

    feat: adiciona repositórios JPA para Usuario e Transacao

---
+++ financecontrol/src/main/java/com/abtiboltech/financecontrol/repository/TipoTransacaoRepository.java
@@ -0,0 +1,7 @@
+package com.abtiboltech.financecontrol.repository;
+
+import com.abtiboltech.financecontrol.entity.TipoTransacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoTransacaoRepository extends JpaRepository<TipoTransacao, Long> {
+}
